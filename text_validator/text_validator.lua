3c4fce3aac078dbc05f973af703e22f136e1cf51
David <j.david82@gmail.com>
2021-01-24 22:33:32 +1100

    testing

--- text_validator/text_validator.lua
+++ text_validator/text_validator.lua
@@ -3,6 +3,9 @@ local M = {}
 local valid_letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_"
 
 function M.validate_display_name_letter(letter)
+	if letter == "%" then
+		return false
+	end
 	return string.find(valid_letters, letter) ~= nil
 end
 
@@ -10,6 +13,9 @@ function M.validate_display_name(display_name)
 	local c
 	for i = 1, string.len(display_name) do
 		c = string.sub(display_name, i, i)
+		if c == "%" then
+			return false
+		end
 		if string.find(valid_letters, c) == nil then
 			return false
 		end
