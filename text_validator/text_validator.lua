94f7a2ad15b94c77c2dcf040297ea8360eb028c6
David <j.david82@gmail.com>
2021-01-24 22:07:44 +1100

    validate display name

--- text_validator/text_validator.lua
+++ text_validator/text_validator.lua
@@ -6,4 +6,13 @@ function M.validate_display_name_letter(letter)
 	return string.find(valid_letters, letter) ~= nil
 end
 
+function M.validate_display_name(display_name)
+	for c in str:gmatch"." do
+		if string.find(valid_letters, c) == nil then
+			return false
+		end
+	end
+	return true
+end
+
 return M
