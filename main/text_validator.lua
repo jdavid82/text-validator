fb5f0f4ef9de7831f401262185977a290b1456a5
David <j.david82@gmail.com>
2021-01-24 21:18:43 +1100

    add defold project

--- main/text_validator.lua
+++ main/text_validator.lua
@@ -3,7 +3,7 @@ local M = {}
 local valid_letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_"
 
 function M.validate_display_name_letter(letter)
-    return string.find(valid_letters, letter) ~= nil
+	return string.find(valid_letters, letter) ~= nil
 end
 
 return M
